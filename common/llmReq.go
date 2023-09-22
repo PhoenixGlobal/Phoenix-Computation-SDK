0e0ab449c4dbd07c6c582bdbaa65aff8f504ce90
zckhub <515588290@qq.com>
2023-09-22 13:18:47 +0800

    add request parameter of create llm job

--- common/llmReq.go
+++ common/llmReq.go
@@ -2,14 +2,19 @@ package common
 
 // ReqLLM  LLM request parameters
 type ReqLLM struct {
-	Prompt  string `json:"prompt" example:"Funniest joke ever:"` //prompt, not empty
-	Temperature float64 `json:"temperature" example:"0.95"`     //temperature, not empty
-	MaxNewTokens int `json:"max_new_tokens" example:"200"`     //tokens, not empty
-	UserToken string `json:"user_token" example:"xxxxxxxxxxxx"`     //user_token, not empty
+	Prompt       string  `json:"prompt" example:"Funniest joke ever:"` //prompt, not empty
+	Temperature  float64 `json:"temperature" example:"0.95"`           //temperature, not empty
+	MaxNewTokens int     `json:"max_new_tokens" example:"200"`         //tokens, not empty
+	UserToken    string  `json:"user_token" example:"xxxxxxxxxxxx"`    //user_token, not empty
 }
 
 type ResLLM struct {
-	Code int `json:"code"`
+	Code int    `json:"code"`
 	Msg  string `json:"msg"`
 	Text string `json:"text"`
+}
+
+type ReqCreateLlmJob struct {
+	JobName string `json:"jobName" example:"Millionaires' Problem"` //job name
+	Count   int64  `json:"count"`                                   //本次购买llm对话次数
 }
--- controllers/llmController_test.go
+++ controllers/llmController_test.go
@@ -7,12 +7,21 @@ import (
 )
 
 func TestCallLLM(t *testing.T) {
-	reqBody:=common.ReqLLM{
+	reqBody := common.ReqLLM{
 		Prompt:       "Funniest joke ever:",
 		Temperature:  0.95,
 		MaxNewTokens: 200,
-		UserToken: "",
+		UserToken:    "",
 	}
-	result,err:=CallLLM(reqBody)
-	fmt.Println(111111,string(result),err)
+	result, err := CallLLM(reqBody)
+	fmt.Println(111111, string(result), err)
+}
+
+func TestCreateLLMJob(t *testing.T) {
+	reqBody := common.ReqCreateLlmJob{JobName: "LLM test11", Count: 1}
+	reqToken := common.ReqGenToken{Email: "515588290@qq.com", Passwd: "111"}
+	token, _ := GenToken(reqToken)
+	fmt.Println("tk=", string(token))
+	res, _ := CreateLLMJob(reqBody, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6IjUxNTU4ODI5MEBxcS5jb20iLCJleHAiOjE2OTUzNjMzODl9.f3uO27mpkAsG-oMgzc4pDuE_sGGrn4UMO7sdH_-xtUU")
+	fmt.Println("res", string(res))
 }
