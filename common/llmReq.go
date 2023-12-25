2d201c800da5591d6353b8f60d4d6e1995271fe7
Kyrie-eng <jiuliang.liu@chinapex.com>
2023-12-25 17:23:09 +0800

    add gen image

--- common/llmReq.go
+++ common/llmReq.go
@@ -8,14 +8,15 @@ type ReqLLM struct {
 	UserToken    string  `json:"user_token" example:"xxxxxxxxxxxx"`    //user_token, not empty
 }
 
-type ResLLM struct {
-	Code int    `json:"code"`
-	Msg  string `json:"msg"`
-	Text string `json:"text"`
-	TokensBalance float64 `json:"tokens_balance"`
-}
-
 type ReqCreateLlmJob struct {
 	JobName   string `json:"jobName" example:"Millionaires' Problem"` //job name
 	TokensNum int64  `json:"tokensNum"`                               //本次购买llm对话次数
+}
+
+// ReqGenImage genImage request parameters
+type ReqGenImage struct {
+	UidName       string `json:"uidname" example:"111@gmail.com"`
+	Prompt        string `json:"prompt"`                            //prompt, not empty
+	NegativePromt string `json:"negative_prompt"`                   //tokens, not empty
+	UserToken     string `json:"user_token" example:"xxxxxxxxxxxx"` //user_token, not empty
 }
--- controllers/llmController.go
+++ controllers/llmController.go
@@ -2,12 +2,14 @@ package controllers
 
 import (
 	"encoding/json"
+
 	"github.com/PhoenixGlobal/Phoenix-Computation-SDK/util"
 
 	"github.com/PhoenixGlobal/Phoenix-Computation-SDK/common"
 )
 
 const CallLLMURL string = "https://www.phoenix.global/sdk/computation/LLM/callLLM"
+const GenImageURL string = "https://www.phoenix.global/sdk/computation/LLM/callGenImage"
 const CreateLLMJobURL string = "https://www.phoenix.global/sdk/computation/LLM/createLLMJob"
 const QueryLLMPriceURL string = "https://www.phoenix.global/sdk/computation/LLM/queryLLMPrice"
 const QueryLLMCountURL string = "https://www.phoenix.global/sdk/computation/LLM/queryLLMActualCount"
@@ -20,7 +22,7 @@ func CallLLM(reqBody common.ReqLLM) (json.RawMessage, error) {
 	if err != nil {
 		return nil, err
 	}
-	result, err := util.SendHttpPost(CallLLMURL, reqJson,reqBody.UserToken)
+	result, err := util.SendHttpPost(CallLLMURL, reqJson, reqBody.UserToken)
 	if err != nil {
 		return nil, err
 	}
@@ -55,23 +57,23 @@ func QueryLLMBuyCount(token string) (json.RawMessage, error) {
 	return result, nil
 }
 
-func GetLLMBuyCount(token string) (count float64,e error)  {
+func GetLLMBuyCount(token string) (count float64, e error) {
 	res, err := QueryLLMBuyCount(token)
-	if err!=nil{
-		e=err
+	if err != nil {
+		e = err
 		return
 	}
 	resultMap := make(map[string]interface{})
 	e = json.Unmarshal(res, &resultMap)
-	if e!=nil{
+	if e != nil {
 		return
 	}
-	dataMap:=resultMap["data"].(map[string]interface{})
+	dataMap := resultMap["data"].(map[string]interface{})
 	count = dataMap["count"].(float64)
 	return
 }
 
-func QueryLLMTokensBalance(token string) (llmTokens float64,e error)  {
+func QueryLLMTokensBalance(token string) (llmTokens float64, e error) {
 	result, err := util.SendHttpGet(QueryLLMTokensBalanceURL, nil, token)
 	if err != nil {
 		e = err
@@ -79,15 +81,15 @@ func QueryLLMTokensBalance(token string) (llmTokens float64,e error)  {
 	}
 	resultMap := make(map[string]interface{})
 	e = json.Unmarshal(result, &resultMap)
-	if e!=nil{
+	if e != nil {
 		return
 	}
-	dataMap:=resultMap["data"].(map[string]interface{})
+	dataMap := resultMap["data"].(map[string]interface{})
 	llmTokens = dataMap["llmTokens"].(float64)
 	return
 }
 
-func QueryLLMFreeTokensBalance(token string) (llmTokens float64,e error)  {
+func QueryLLMFreeTokensBalance(token string) (llmTokens float64, e error) {
 	result, err := util.SendHttpGet(QueryLLMFreeTokensBalanceURL, nil, token)
 	if err != nil {
 		e = err
@@ -95,10 +97,23 @@ func QueryLLMFreeTokensBalance(token string) (llmTokens float64,e error)  {
 	}
 	resultMap := make(map[string]interface{})
 	e = json.Unmarshal(result, &resultMap)
-	if e!=nil{
+	if e != nil {
 		return
 	}
-	dataMap:=resultMap["data"].(map[string]interface{})
+	dataMap := resultMap["data"].(map[string]interface{})
 	llmTokens = dataMap["llmTokens"].(float64)
 	return
+}
+
+// GenImage  call LLM api
+func GenImage(reqBody common.ReqGenImage) (json.RawMessage, error) {
+	reqJson, err := json.Marshal(reqBody)
+	if err != nil {
+		return nil, err
+	}
+	result, err := util.SendHttpPost(GenImageURL, reqJson, reqBody.UserToken)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
 }
