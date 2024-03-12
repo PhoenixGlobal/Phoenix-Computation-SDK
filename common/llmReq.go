66716ad132f6dce86ec4f3a5471e5b5e2e8dd853
Kyrie-eng <jiuliang.liu@chinapex.com>
2024-03-12 17:51:53 +0800

    update ReqGenImage

--- common/llmReq.go
+++ common/llmReq.go
@@ -15,9 +15,19 @@ type ReqCreateLlmJob struct {
 
 // ReqGenImage genImage request parameters
 type ReqGenImage struct {
-	Prompt        string `json:"prompt"`                            //prompt, not empty
-	NegativePromt string `json:"negative_prompt"`                   //tokens, not empty
-	UserToken     string `json:"user_token" example:"xxxxxxxxxxxx"` //user_token, not empty
+	Prompt               string  `json:"prompt"`                            //prompt, not empty
+	NegativePromt        string  `json:"negative_prompt"`                   //tokens, not empty
+	UserToken            string  `json:"user_token" example:"xxxxxxxxxxxx"` //user_token, not empty
+	NSteps               int     `json:"n_steps" example:"20"`
+	GuidanceScale        float64 `json:"guidance_scale" example:"8"` // float, 0 to 10
+	Seed                 int     `json:"seed" example:"1"`           // int, >0
+	PipeName             string  `json:"pipe_name" example:"txt2img"`
+	LoraPathList         string  `json:"lora_path_list" example:"https://xxxx"`
+	LoraWeightList       string  `json:"lora_weight_list" example:"0.8"`
+	TextualInversionPath string  `json:"textual_inversion_path" example:"without"`
+	LoraScale            float64 `json:"lora_scale" example:"0.8"`
+	Size                 string  `json:"size" example:"512^*^512"`
+	ClipSkip             int     `json:"clip_skip" example:"1"`
 }
 
 // ReqGenSDXLImage gen SDXL image request parameters
